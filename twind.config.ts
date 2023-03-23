45a608f408fe9cae39ab6be0e024baa3dcc24586
Guilherme Canella <guilherme.canella@corebiz.ag>
2023-03-23 08:27:10 -0300

    first configs

--- twind.config.ts
+++ twind.config.ts
@@ -3,17 +3,18 @@ export default {
   theme: {
     extend: {
       screens: {
-        sm: "640px",
-        md: "768px",
-        lg: "1024px",
-        xl: "1280px",
-        "2xl": "1536px",
+        mobile: "360px",
+        tablet: "600px",
+        small: "900px",
+        medium: "1200px",
+        large: "1800px",
       },
       colors: {
         primary: "#2FD180",
-        "primary-dark": "#003232",
+        "primary-dark": "#000000",
         "primary-light": "#C5FFE9",
         transparent: "transparent",
+        "primary-red": "#B92C38",
       },
       fontFamily: {
         sans: ["Albert Sans", "sans-serif"],
